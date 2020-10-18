c73aef43bd166ca50a6e8a2fdbebcd22c3759744
Vadim Chernyshov <VChernyshoff@mail.ru>
2020-10-18 23:23:31 +0300

    changed logic. Added main funcction for threading

--- vchernyshoff/task_3/main.py
+++ vchernyshoff/task_3/main.py
@@ -1,13 +1,17 @@
 import time
 import requests
-from langdetect import detect
 from pathlib import Path
 import threading
 import argparse
 
 parser = argparse.ArgumentParser(description='Incredible parser wiki. It shows count of all symbols from asked wiki http(s) pages.')
-parser.add_help
-all_symbols = ''
+parser = argparse.ArgumentParser(description='Videos to images')
+parser.add_argument('output-file-name', type=str, help='set name of an output file')
+parser.add_argument('script-mode{threaded, single}', type=str, help='Script run mode')
+parser.add_argument('threads_count{true, false}', type=str, help='Show count of threads')
+args = parser.parse_args()
+
+symbol_count = {}
 links = (
     'https://en.wikipedia.org/wiki/Wikipedia',
     'https://en.wikipedia.org/wiki/Main_Page',
@@ -22,23 +26,10 @@ links = (
     )
 
 
+# ================ with no threading: ==========================================
 def get_symbols_from_link(link: str) -> str:
     """get html from link"""
     return requests.get(link).text
-
-
-def threading_get_symbols_from_link(link: str) -> None:
-    """get html from link"""
-    global all_symbols
-    all_symbols = requests.get(link).text
-
-
-def get_symbol_set(all_symbols: str) -> set:
-    symbol_set = set()
-
-    for symbol in all_symbols:
-        symbol_set.add(symbol)
-    return symbol_set
 
 
 def sort_dict(symbol_count: dict) -> str:
@@ -46,59 +37,78 @@ def sort_dict(symbol_count: dict) -> str:
     :return: sorted dict"""
     symbol_count_sorted = ''
     for key, val in sorted(symbol_count.items(), key=lambda x: x[1]):
-        try:
-            if detect(key) == 'fa' or detect(val) == 'ur' or detect(val) == 'he' or detect(val) == 'ar':    # hebrew
-                # and other langs works from right to left. We should swap it: "ޑ": 1"
-                # perhaps, it would be the best way, use unicode or ASCII codes
-                symbol_count_sorted += f'{val} :"{key}"'
-                symbol_count_sorted += '\n'
-                continue
-        except:
-            pass
         symbol_count_sorted += f'"{key}": {val}\n'
-
     return symbol_count_sorted
 
 
-def main() -> None:
-    # # 1) Download all http text with no threading
-    # all_symbols = ''
-    # for link in links:
-    #     all_symbols += get_symbols_from_link(link)
+def export_to_file(text: str) -> None:
+    """save text to file symbol_count.txt"""
+    p = Path('symbol_count.txt')
+    p.write_text(text)
 
-    # 1.1) Download all http text with threading
-    global all_symbols
+
+
+def main_no_threading() -> None:
+    # 1) Download all http text with no threading
+    all_symbols = ''
     for link in links:
-        thread = threading.Thread(target=threading_get_symbols_from_link, args=(link, ))
-        thread.start()
-        while threading.active_count() > 1:
-            time.sleep(0.05)
-            pass
-    # 2) Let's create set with symbols which persist in the text and create dict:
-    symbol_count = {}
-    symbol_set = get_symbol_set(all_symbols)
-    for symbol in symbol_set:
-        symbol_count[symbol] = 0
+        all_symbols += get_symbols_from_link(link)
 
-    # 3) Now let's count every symbol
+
+    # 2) Check every symbol, and create dict with symbol (as ) count
+    symbol_count = {}
     for symbol in all_symbols:
-        symbol_count[symbol] += 1
+        try:
+            symbol_count[symbol] += 1
+        except:
+            symbol_count[symbol] = 1
 
     # 4) Sort dict by values and convert to str:
     symbol_count_sorted = sort_dict(symbol_count)
 
     # 5) export dit to file symbol_count.txt
-    p = Path('symbol_count.txt')
-    p.write_text(symbol_count_sorted)
+    export_to_file(symbol_count_sorted)
+
+
+# ======================with threading:==========================================
+def threading_get_symbols_from_link(link: str) -> None:
+    """get html from link"""
+    global symbol_count
+
+    text = requests.get(link).text  # get text from wiki
+    for symbol in text:
+        try:
+            symbol_count[symbol] += 1
+        except:
+            symbol_count[symbol] = 1
+
+
+def main_with_threadng():
+    global symbol_count
+    # 1) Download all http text with threading and create set:
+    for link in links:
+        thread = threading.Thread(target=threading_get_symbols_from_link, args=(link, ))
+        thread.start()
+
+    while threading.active_count() > 1:
+        time.sleep(0.01)
+
+    # we have symbol count set. Let's sort it
+    symbol_count_sorted = sort_dict(symbol_count)
+
+    # export it:
+    export_to_file(symbol_count_sorted)
 
 
 if __name__ == '__main__':
     start_time = time.time()
-    main()
+    # if threading_switch is True:
+    #     main_with_threading()
+    # else:
+    #     main_no_threading()
+
+    main_no_threading()
+    # main_with_threadng()
+
     print('Затраченное время: ' + format(time.time() - start_time))
-
-
-    # a = detect('ޑ')
-    # print(a)
-
 
