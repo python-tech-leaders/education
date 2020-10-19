8aaa75e12b856bc92711a3011d420fac84a69534
hasska322 <hasska322@gmail.com>
2020-10-19 19:43:30 +0300

    threading

--- andrey_n/task_3/task_3.py
+++ andrey_n/task_3/task_3.py
@@ -1,10 +1,13 @@
 import time
 import requests
-from collections import Counter
+import threading
 import pathlib
 import argparse
+import queue
+from collections import Counter
 
 
+#  START settings
 TARGET = ["https://en.wikipedia.org/wiki/Wikipedia",
               "https://en.wikipedia.org/wiki/Main_Page",
               "https://en.wikipedia.org/wiki/COVID-19_pandemic",
@@ -14,11 +17,30 @@ TARGET = ["https://en.wikipedia.org/wiki/Wikipedia",
               "https://uk.wikipedia.org/wiki/Пандемія_коронавірусної_хвороби_2019",
               "https://bg.wikipedia.org/wiki/Пандемия_от_коронавирус_(2019_–_2020)",
               "https://be.wikipedia.org/wiki/Пандэмія_COVID-19",
-              "https://ro.wikipedia.org/wiki/Pandemia_de_coronaviroză_(COVID-19)",]
+              "https://ro.wikipedia.org/wiki/Pandemia_de_coronaviroză_(COVID-19)", ]
+
+parser = argparse.ArgumentParser(description='Working with URLS')
+parser.add_argument(
+    '--mode',
+    default='single',
+    choices=('single', 'threading'),
+    help='Mode for script (Single or threading)',
+)
+parser.add_argument(
+    '--file_name',
+    default=pathlib.Path('res.txt'),
+    help='Write result to .txt in folder',
+)
+parser.add_argument(
+    '--threads',
+    default=3,
+    help='How many threads you need? Write a number.',
+)
+#  END settings
 
 
-#  Parsing text from site
-def parser(url: str) -> str:
+#  START Parsing text from site
+def parse(url: str) -> str:
     """
     Parsing content from url
     :param url - url address of website
@@ -26,7 +48,7 @@ def parser(url: str) -> str:
     """
     data = ''
     try:
-        response = requests.request(url=url, method='POST')
+        response = requests.request(url=url, method='GET')
         response.raise_for_status()
         data = str(response.content)
     except requests.exceptions.HTTPError as err_h:
@@ -41,8 +63,8 @@ def parser(url: str) -> str:
     return data
 
 
-#  Parsing without threading
-def single_parsing(urls: list):
+#  single
+def single_parsing(urls: list) -> list:
     """
     Parser without threading
     :param urls:
@@ -51,30 +73,77 @@ def single_parsing(urls: list):
               Sorted list of all chars with counts
     """
 
-    start_time = time.time()
     result = ''
     for url in urls:
-        result += parser(url)
+        result += parse(url)
     len_of_text = len(result)
-    single_time = time.time() - start_time
     cnt = sorted(dict(Counter(result)).items(), key=lambda item: item[1], reverse=True)
-    print(f"len: {len_of_text}\n"
-          f"time: {single_time}\n"
-          f"{cnt}")
-    return [len_of_text, single_time, cnt]
+
+    return [len_of_text, cnt]
 
 
-#  Write to file
-def log_to_file(data: list, file_name: str):
+# threadings
+
+def thread_data(qu, urls):
+    for url in urls:
+        qu.put(parse(url))
+
+
+def thread_parse(urls: list, threads: int) -> list:
+    q = queue.Queue()
+    for i in range(threads):
+        start = i * len(urls) // threads
+        end = (i+1) * len(urls) // threads
+        thread_i = threading.Thread(target=thread_data, args=(q, urls[start:end]))
+        thread_i.start()
+        print(thread_i.name)
+        thread_i.join()
+    res = q.get()
+    cnt = sorted(dict(Counter(res)).items(), key=lambda item: item[1], reverse=True)
+    print(f"All chars: {len(res)}")
+
+    return [len(res), cnt]
+#  END
+
+
+#  START config(write to file)
+def log_to_file(data: list, full_time: time, file_name: str) -> None:
     """
     Write to file using pathlib
+    :param full_time:
     :param file_name:
     :param data:
     :return:
     """
 
     path = pathlib.Path(file_name)
+    with path.open('w', encoding='utf8') as file:
+        print(f"All chars: {data[0]}\n Time for parsing: {full_time}", file=file)
+        for res in data[1]:
+            print(res, file=file)
 
 
-single_parsing(TARGET)
+#  run scripts
+def main():
+    """
+    run script
+    :return:
+    """
+    start_time = time.time()
+    args = parser.parse_args()
+    if args.mode == 'single':
+        log_to_file(data=single_parsing(TARGET),
+                    full_time=time.time() - start_time,
+                    file_name=args.file_name)
+    elif args.mode == 'threading':
+        log_to_file(data=thread_parse(TARGET, int(args.threads)),
+                    full_time=time.time() - start_time,
+                    file_name=args.file_name
+                    )
+# END
+
+
+if __name__ == "__main__":
+    main()
+
 
