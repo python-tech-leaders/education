0f5eb66f1c8a07227098fb5017f2fb9a99c392d3
arc-lena <kolenat2002!@gmail.com>
2020-10-23 16:52:27 +0300

    made task4

--- lena/homework_4/HW4/HW4/spiders/crawler.py
+++ lena/homework_4/HW4/HW4/spiders/crawler.py
@@ -1,11 +1,20 @@
 import scrapy
-
+from .. items import tem
+from scrapy.exceptions import CloseSpider
 
 class OLXSpider(scrapy.Spider):
     name = "olx_el"
+    allowed_domains = ['olx.ua']
     start_urls = [
         'https://www.olx.ua/uk/elektronika/',
     ]
+    custom_settings = {
+        'FEED_URI': 'output.json',
+        'FEED_EXPORT_ENCODING': 'utf-8',
+        'CLOSESPIDER_ITEMCOUNT': 100,
+    }
+
+    count = 0
 
     def parse(self, response):
         l = []
@@ -13,29 +22,44 @@ class OLXSpider(scrapy.Spider):
             url = response.urljoin(link)
             l.append(url)
         for link_prod in l:
-            yield scrapy.Request(link_prod, callback=self.parse2)
+            yield scrapy.Request(link_prod, callback=self.parse0)
+        pagination_links = response.css('span.fleft a')
+        yield from response.follow_all(pagination_links, self.parse)
+
+    def parse0(self, response):
+        if self.count<100:
+            for info in response.css("div.offerdescription"):
+                product_price = info.css('div strong::text').get()
+                product_name = (info.css('div h1::text').get())[9:-3]
+                product_category = (info.css('a strong::text').getall())[1]
+                product_photos = info.css('div img::attr(src)').get(default= "")
+                product_description = info.css('#textContent::text').get(default='').strip()
+                product_id = info.css('.offer-bottombar__item~ .offer-bottombar__item+ .offer-bottombar__item strong::text').get(default='').strip()
+                produc_date_of_creation = info.css('em strong::text').get()
+                product_views_count = info.css('span strong::text').get()
+                olx_delivery_availability = info.css('.AdPageBox__link-button-caption::text').get(default='').strip()
+                urls_product = info.xpath('/html/head/link[1]/@href').get()
+                seller_name = info.css('h4 a::text').get(default = '').strip()
+                seller_address = info.xpath('//*[@id="offeractions"]/div[4]/div[2]/div[1]/address/p/text()').get()
+
+                product = tem()
+
+                product['product_price'] = product_price
+                product['product_name'] = product_name
+                product['product_category'] = product_category
+                product['product_photos'] = product_photos
+                product['product_description'] = product_description
+                product['product_id'] = product_id
+                product['produc_date_of_creation'] = produc_date_of_creation
+                product['product_views_count'] = product_views_count
+                product['olx_delivery_availability'] = olx_delivery_availability
+                product['urls_product'] = urls_product
+                product['seller_name'] = seller_name
+                product['seller_address'] = seller_address
 
 
-
-    def parse2(self, response):
-        for info in response.css("div.offerdescription"):
-            temp = info.css('li span::text').getall()
-            c = False
-            if '\n\t\t\t\tOLX Доставка\t\t\t' in temp:
-                c = True
-            inf = info.css('div[class="clr lheight20 large"]').get()
-
-
-            yield {
-                'product price': float(((info.css('div strong::text').get())[:-4]).replace(" ", "")),
-                'product currency': ((info.css('div strong::text').get())[-4:]),
-                'product name': (info.css('div h1::text').get())[9:-3],
-                'product category': (info.css('a strong::text').getall())[1],
-                'product photos urls': info.css('div img::attr(src)').get(default= ""),
-                'product state': info.css('adress p::text').get(),
-                'product description': inf,
-                'product date of creation': info.css('em strong::text').get(),
-                'product views count': info.css('span strong::text').get(),
-                'product id': (info.css('li strong::text').getall())[-1],
-                'olx delivery availability': c,
-            }
+                self.count += 1
+                print(self.count)
+                yield product
+                if self.count == 100:
+                    raise CloseSpider()
