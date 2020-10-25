51771550d483524f9adf839eb22ae711a05b447c
Vadim Chernyshov <VChernyshoff@mail.ru>
2020-10-25 10:51:33 +0300

    homework 4

---
+++ vchernyshoff/homework_4/olx_scraper/olx_scraper/items.py
@@ -0,0 +1,24 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy import Field
+
+
+class OlxScraperItem(scrapy.Item):
+    name = Field()
+    url = Field()
+    price = Field()
+    currency = Field()
+    category = Field()
+    state = Field()
+    description = Field()
+    date_of_creation = Field()
+    views_count = Field()
+    id = Field()
+    photos_urls = Field()
+    seller_name = Field()
+    seller_address = Field()
+    olx_delivery_availability = Field()
