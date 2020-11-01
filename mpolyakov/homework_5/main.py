d4767ce96c7a38b0f5b11528044e65b1defd3d3d
Максим <maksim@MacBook-Pro-Maria.local>
2020-11-02 00:04:59 +0300

    add functions: strip_comments, format_duration, is_isogram

---
+++ mpolyakov/homework_5/main.py
@@ -0,0 +1,130 @@
+from collections import Counter
+
+
+def double_input(input_data):
+    """Doubles input"""
+    return input_data * 2
+
+
+def find_start(input_string, markers):
+    for index, item in enumerate(input_string):
+        if item in markers:
+            return index
+
+
+def find_stop(input_string):
+    for index, item in enumerate(input_string):
+        if item == "\n":
+            return index
+
+
+def strip_comments(input_string, markers):
+    """
+    Complete the solution so that it strips all text that follows any of a set of comment markers passed in.
+    Any whitespace at the end of the line should also be stripped out.
+
+    Example:
+
+    Input string:
+
+    "
+    apples, pears # and bananas
+    grapes
+    bananas !apples
+    "
+
+    Markers: ["#", "!"]
+
+    The output expected would be:
+
+    "
+    apples, pears
+    grapes
+    bananas
+    "
+
+    result = strip_comments("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"])
+    # "apples, pears\ngrapes\nbananas"
+    """
+    result = input_string[:] + "\n"
+    for marker in markers:
+        cursor = iter(result)
+        start = find_start(cursor, marker)
+        stop = find_stop(cursor) + start + 1
+        result = result[:start].strip() + result[stop:]
+    return result.strip()
+
+
+def format_duration(input_seconds):
+    """
+    Your task is to write a function which formats a duration, given as a number of seconds, in a human-friendly way.
+    The function must accept a non-negative integer. If it is zero, it just returns "now". Otherwise, the duration is expressed as a combination of years, days, hours, minutes and seconds.
+
+    Examples:
+
+    format_duration(62)    # returns "1 minute and 2 seconds"
+    format_duration(3721)  # returns "1 hour, 2 minutes and 1 second"
+
+    Assume that year is 365 days
+    """
+    if input_seconds == 0:
+        return "now"
+    minutes, seconds = divmod(input_seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    years, days = divmod(days, 365)
+    seconds_text = "seconds" if seconds > 1 else "second"
+    minutes_text = "minutes" if minutes > 1 else "minute"
+    hours_text = "hours" if hours > 1 else "hour"
+    days_text = "days" if days > 1 else "day"
+    years_text = "years" if years > 1 else "year"
+    if years:
+        return f"{years} {years_text}, {days} {days_text}, {hours} {hours_text}, {minutes} {minutes_text} and {seconds} {seconds_text}"
+    elif days:
+        return f"{days} {days_text}, {hours} {hours_text}, {minutes} {minutes_text} and {seconds} {seconds_text}"
+    elif hours:
+        return f"{hours} {hours_text}, {minutes} {minutes_text} and {seconds} {seconds_text}"
+    elif minutes > 1:
+        return f"{minutes} {minutes_text} and {seconds} {seconds_text}"
+    elif minutes == 1:
+        return f"{minutes} {minutes_text} and {seconds} {seconds_text}"
+
+
+def is_isogram(input_string):
+    """
+    An isogram is a word that has no repeating letters, consecutive or non-consecutive.
+    Implement a function that determines whether a string that contains only letters is an isogram.
+    Assume the empty string is an isogram. Ignore letter case.
+
+    is_isogram("Dermatoglyphics" ) == true
+    is_isogram("aba" ) == false
+    is_isogram("moOse" ) == false # -- ignore letter case
+    """
+    counter = Counter(input_string.lower())
+    return len(list(counter)) == len(input_string)
+
+
+def snail(input_array):
+    """
+    Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
+
+    array = [[1,2,3],
+             [4,5,6],
+             [7,8,9]]
+    snail(array) #=> [1,2,3,6,9,8,7,4,5]
+    For better understanding, please follow the numbers of the next array consecutively:
+
+    array = [[1,2,3],
+             [8,9,4],
+             [7,6,5]]
+    snail(array) #=> [1,2,3,4,5,6,7,8,9]
+
+    For better understanding: http://www.haan.lu/files/2513/8347/2456/snail.png
+    """
+    pass
+
+
+"""
+Lazy init
+https://www.codewars.com/kata/59b7b43b4f98a81b2d00000a
+"""
---
+++ mpolyakov/homework_5/tests/test_main.py
@@ -0,0 +1,61 @@
+import pytest
+
+import main
+
+
+@pytest.mark.parametrize(
+    "input_data,expected",
+    [(10, 20), ([1, 2], [1, 2, 1, 2])],
+)
+def test_double_input(input_data, expected):
+    """Test double_input function"""
+    assert main.double_input(input_data) == expected
+
+
+def test_double_input_raises():
+    """Test double_input function raises exception"""
+    with pytest.raises(TypeError):
+        main.double_input(list)
+
+
+@pytest.mark.parametrize(
+    "input_data,expected",
+    [("Dermatoglyphics", True), ("aba", False), ("moOse", False)],
+)
+def test_is_isogram(input_data, expected):
+    assert main.is_isogram(input_data) == expected
+
+
+@pytest.mark.parametrize(
+    "input_data,expected",
+    [
+        (62, "1 minute and 2 seconds"),
+        (3721, "1 hour, 2 minutes and 1 second"),
+        (103721, "1 day, 4 hours, 48 minutes and 41 seconds"),
+        (3333721, "38 days, 14 hours, 2 minutes and 1 second"),
+        (33333721, "1 year, 20 days, 19 hours, 22 minutes and 1 second"),
+        (99993721, "3 years, 62 days, 8 hours, 2 minutes and 1 second"),
+    ],
+)
+def test_format_duration(input_data, expected):
+    assert main.format_duration(input_data) == expected
+
+
+@pytest.mark.parametrize(
+    "input_data,expected",
+    [
+        (
+            ("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"]),
+            "apples, pears\ngrapes\nbananas",
+        ),
+        (
+            (
+                "  apples, pears # and bla-bla\ngrapes ?hello\nbananas !apples.  ",
+                ["#", "!", "?"],
+            ),
+            "apples, pears\ngrapes\nbananas",
+        ),
+    ],
+)
+def test_strip_comments(input_data, expected):
+    assert main.strip_comments(input_data[0], input_data[1]) == expected
